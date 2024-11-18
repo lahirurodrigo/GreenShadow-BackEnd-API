f46b098ed87a623868a18664aa2a78a0a763b582
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-11-18 05:41:55 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/greenshadowbackendapi/GreenShadowBackEndApiApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadowbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowBackEndApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowBackEndApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/greenshadowbackendapi/GreenShadowBackEndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadowbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowBackEndApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
