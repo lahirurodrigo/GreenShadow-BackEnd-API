dc2a2d94986d5f3f79c221f3759754b27e0677a0
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-02 10:38:46 +0530

    refactor SelectedEntityErrorStatus

--- src/main/java/lk/ijse/greenshadowbackendapi/customStatusCode/SelectedEntityErrorStatus.java
+++ src/main/java/lk/ijse/greenshadowbackendapi/customStatusCode/SelectedEntityErrorStatus.java
@@ -3,6 +3,7 @@ package lk.ijse.greenshadowbackendapi.customStatusCode;
 import lk.ijse.greenshadowbackendapi.dto.CropStatus;
 import lk.ijse.greenshadowbackendapi.dto.FieldStatus;
 import lk.ijse.greenshadowbackendapi.dto.StaffStatus;
+import lk.ijse.greenshadowbackendapi.dto.VehicleStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class SelectedEntityErrorStatus implements CropStatus, FieldStatus, StaffStatus {
+public class SelectedEntityErrorStatus implements CropStatus, FieldStatus, StaffStatus, VehicleStatus {
     private int statusCode;
     private String statusMessage;
 }
