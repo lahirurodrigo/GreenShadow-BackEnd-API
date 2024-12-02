82d2e6ae394a763966273cd7bb845c16367dc70d
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-02 10:32:42 +0530

    add save method to Vehicle Service

--- src/main/java/lk/ijse/greenshadowbackendapi/service/VehicleService.java
+++ src/main/java/lk/ijse/greenshadowbackendapi/service/VehicleService.java
@@ -1,4 +1,8 @@
 package lk.ijse.greenshadowbackendapi.service;
 
+import lk.ijse.greenshadowbackendapi.dto.impl.VehicleDTO;
+import lk.ijse.greenshadowbackendapi.exception.DataPersistException;
+
 public interface VehicleService {
+    void saveVehicle(VehicleDTO vehicleDTO) throws DataPersistException;
 }
--- src/main/java/lk/ijse/greenshadowbackendapi/service/impl/VehicleServiceImpl.java
+++ src/main/java/lk/ijse/greenshadowbackendapi/service/impl/VehicleServiceImpl.java
@@ -1,10 +1,40 @@
 package lk.ijse.greenshadowbackendapi.service.impl;
 
+import lk.ijse.greenshadowbackendapi.dao.VehicleDAO;
+import lk.ijse.greenshadowbackendapi.dto.impl.VehicleDTO;
+import lk.ijse.greenshadowbackendapi.entity.VehicleEntity;
+import lk.ijse.greenshadowbackendapi.exception.DataPersistException;
 import lk.ijse.greenshadowbackendapi.service.VehicleService;
+import lk.ijse.greenshadowbackendapi.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class VehicleServiceImpl implements VehicleService {
+
+    @Autowired
+    private VehicleDAO vehicleDAO;
+
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public void saveVehicle(VehicleDTO vehicleDTO) throws DataPersistException {
+        // Check if the vehicleCode already exists
+        if (vehicleDAO.existsById(vehicleDTO.getVehicleCode())) {
+            throw new DataPersistException("Vehicle with the same code already exists!");
+        }
+
+        // Convert DTO to Entity
+        VehicleEntity vehicleEntity = mapping.toVehicleEntity(vehicleDTO);
+
+        // Save the vehicle entity
+        VehicleEntity savedVehicle = vehicleDAO.save(vehicleEntity);
+        if (savedVehicle == null) {
+            throw new DataPersistException("Vehicle not saved!");
+        }
+    }
+
 }
