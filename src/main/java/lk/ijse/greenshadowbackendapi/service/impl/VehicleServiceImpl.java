ccc372f2a7eaa88f2b8995a405118dc7c2c39b2b
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-02 10:29:31 +0530

    add requored annotations for VehicleServiceImpl

--- src/main/java/lk/ijse/greenshadowbackendapi/service/impl/VehicleServiceImpl.java
+++ src/main/java/lk/ijse/greenshadowbackendapi/service/impl/VehicleServiceImpl.java
@@ -1,6 +1,10 @@
 package lk.ijse.greenshadowbackendapi.service.impl;
 
 import lk.ijse.greenshadowbackendapi.service.VehicleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class VehicleServiceImpl implements VehicleService {
 }
