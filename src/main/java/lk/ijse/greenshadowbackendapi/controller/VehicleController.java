b538a5b3b858f08ae4f922888915387ea646fb1b
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-02 11:01:39 +0530

    add POST endpoint to save vehicle details

--- src/main/java/lk/ijse/greenshadowbackendapi/controller/VehicleController.java
+++ src/main/java/lk/ijse/greenshadowbackendapi/controller/VehicleController.java
@@ -1,10 +1,13 @@
 package lk.ijse.greenshadowbackendapi.controller;
 
+import lk.ijse.greenshadowbackendapi.dto.impl.VehicleDTO;
+import lk.ijse.greenshadowbackendapi.exception.DataPersistException;
 import lk.ijse.greenshadowbackendapi.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/vehicle")
@@ -13,4 +16,19 @@ public class VehicleController {
 
     @Autowired
     private VehicleService vehicleService;
+
+    // Save vehicle details
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VehicleDTO> saveVehicle(@RequestBody VehicleDTO vehicleDTO) {
+        try {
+            // Save vehicle details
+            System.out.println("Received vehicleCode: " + vehicleDTO.getVehicleCode());
+            vehicleService.saveVehicle(vehicleDTO);
+            return new ResponseEntity<>(vehicleDTO, HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
