9b755765cfd2693062f1edc8572e326b3a79bd00
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-02 10:58:29 +0530

    inject VehicleService to VehicleController

--- src/main/java/lk/ijse/greenshadowbackendapi/controller/VehicleController.java
+++ src/main/java/lk/ijse/greenshadowbackendapi/controller/VehicleController.java
@@ -1,5 +1,7 @@
 package lk.ijse.greenshadowbackendapi.controller;
 
+import lk.ijse.greenshadowbackendapi.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/vehicle")
 @CrossOrigin
 public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
 }
