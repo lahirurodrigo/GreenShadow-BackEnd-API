d57eb244fd366958dd0a8b6032fbfb17228e7e76
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-02 20:37:54 +0530

    refactor FieldStaffController

--- src/main/java/lk/ijse/greenshadowbackendapi/controller/FieldStaffController.java
+++ src/main/java/lk/ijse/greenshadowbackendapi/controller/FieldStaffController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/v1/fields/{fieldId}/staff")
+@RequestMapping("api/v1/field-staff")
 @CrossOrigin
 public class FieldStaffController {
 
