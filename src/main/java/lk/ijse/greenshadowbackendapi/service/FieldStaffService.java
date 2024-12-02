bf32ec22d10b235cdfaf0a45326ff2a759d3f7e8
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-02 18:57:36 +0530

    add FieldStaffService, FieldStaffServiceImpl

---
+++ src/main/java/lk/ijse/greenshadowbackendapi/service/FieldStaffService.java
@@ -0,0 +1,4 @@
+package lk.ijse.greenshadowbackendapi.service;
+
+public interface FieldStaffService {
+}
---
+++ src/main/java/lk/ijse/greenshadowbackendapi/service/impl/FieldStaffServiceImpl.java
@@ -0,0 +1,6 @@
+package lk.ijse.greenshadowbackendapi.service.impl;
+
+import lk.ijse.greenshadowbackendapi.service.FieldStaffService;
+
+public class FieldStaffServiceImpl implements FieldStaffService {
+}
