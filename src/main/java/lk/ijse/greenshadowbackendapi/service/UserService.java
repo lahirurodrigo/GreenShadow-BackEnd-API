1e0d54a11135368ba15a5b694983741add7a19db
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-12-05 21:38:29 +0530

    create UserService, UserServiceImpl

---
+++ src/main/java/lk/ijse/greenshadowbackendapi/service/UserService.java
@@ -0,0 +1,18 @@
+package lk.ijse.greenshadowbackendapi.service;
+
+import lk.ijse.greenshadowbackendapi.entity.UserEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<UserEntity> getAllUsers();
+
+    Optional<UserEntity> getUserByEmail(String email);
+
+    void saveUser(UserEntity userEntity);
+
+    void updateUser(String email, UserEntity userEntity);
+
+    void deleteUser(String email);
+}
---
+++ src/main/java/lk/ijse/greenshadowbackendapi/service/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package lk.ijse.greenshadowbackendapi.service.impl;
+
+import lk.ijse.greenshadowbackendapi.dao.UserDAO;
+import lk.ijse.greenshadowbackendapi.entity.UserEntity;
+import lk.ijse.greenshadowbackendapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userRepository;
+
+    @Override
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserEntity> getUserByEmail(String email) {
+        return userRepository.findById(email);
+    }
+
+    @Override
+    public void saveUser(UserEntity userEntity) {
+        userRepository.save(userEntity);
+    }
+
+    @Override
+    public void updateUser(String email, UserEntity userEntity) {
+        if (userRepository.existsById(email)) {
+            userRepository.save(userEntity);
+        } else {
+            throw new RuntimeException("User with email " + email + " not found!");
+        }
+    }
+
+    @Override
+    public void deleteUser(String email) {
+        if (userRepository.existsById(email)) {
+            userRepository.deleteById(email);
+        } else {
+            throw new RuntimeException("User with email " + email + " not found!");
+        }
+    }
+}
